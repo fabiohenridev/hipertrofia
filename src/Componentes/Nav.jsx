37e0a597d987dbc114d21673361f6338bfc88b7b
fabio henrique <henri8274@gmail.com>
2024-11-30 13:53:12 -0300

    change-button

--- src/Componentes/Nav.jsx
+++ src/Componentes/Nav.jsx
@@ -4,6 +4,7 @@ export default function Nav(){
         <div className='Nav'>
            <h2>Garanta já o e-book por apenas <span className='valor'>$1,99</span>!</h2>
            <p>CLIQUE JÁ NO BOTÃO DO WHATSAPP PARA TER ACESSO AO MEIO DE PAGAMENTO VIA PIX</p>
+           <span><i class="bi bi-arrow-down-circle-fill Flecha"></i></span>
            <div className='BotaoPix'>
            <button onClick={()=>{botaoWhatsapp()}}><i class="bi bi-whatsapp whatsapp"></i></button>
            <h3>PIX</h3>
